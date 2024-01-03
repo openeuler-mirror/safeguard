3a5fca66126ef629987218f7b83c18ae3d499986
Ren MinMin <tongyx12@chinaunicom.cn>
2024-01-03 16:37:11 +0800

    replace bpf_d_path function

--- pkg/bpf/c/restricted-file.bpf.c
+++ pkg/bpf/c/restricted-file.bpf.c
@@ -46,6 +46,129 @@ BPF_HASH(fileopen_safeguard_config_map, u32, struct fileopen_safeguard_config, 2
 BPF_HASH(allowed_access_files, u32, struct file_path, 256);
 BPF_HASH(denied_access_files, u32, struct file_path, 256);
 
+#define EFAULT 14 /* Bad address */
+
+#define MAX_PATH_SIZE 4096 // PATH_MAX from <linux/limits.h>
+#define LIMIT_PATH_SIZE(x) ((x) & (MAX_PATH_SIZE - 1))
+#define MAX_PATH_COMPONENTS 20
+
+#define MAX_PERCPU_ARRAY_SIZE (1 << 15)
+#define HALF_PERCPU_ARRAY_SIZE (MAX_PERCPU_ARRAY_SIZE >> 1)
+#define LIMIT_PERCPU_ARRAY_SIZE(x) ((x) & (MAX_PERCPU_ARRAY_SIZE - 1))
+#define LIMIT_HALF_PERCPU_ARRAY_SIZE(x) ((x) & (HALF_PERCPU_ARRAY_SIZE - 1))
+
+struct buffer {
+  u8 data[MAX_PERCPU_ARRAY_SIZE];
+};
+
+struct {
+  __uint(type, BPF_MAP_TYPE_PERCPU_ARRAY);
+  __type(key, u32);
+  __type(value, struct buffer);
+  __uint(max_entries, 1);
+} heaps_map SEC(".maps");
+
+static struct buffer *get_buffer() {
+  u32 zero = 0;
+  return (struct buffer *)bpf_map_lookup_elem(&heaps_map, &zero);
+}
+
+static long get_path_str_from_path(u_char **path_str, struct path *path, struct buffer *out_buf) {
+
+  long ret;
+  struct dentry *dentry, *dentry_parent, *dentry_mnt;
+  struct vfsmount *vfsmnt;
+  struct mount *mnt, *mnt_parent;
+  const u_char *name;
+  size_t name_len;
+
+  dentry = BPF_CORE_READ(path, dentry);
+  vfsmnt = BPF_CORE_READ(path, mnt);
+  mnt = container_of(vfsmnt, struct mount, mnt);
+  mnt_parent = BPF_CORE_READ(mnt, mnt_parent);
+
+  size_t buf_off = HALF_PERCPU_ARRAY_SIZE;
+
+#pragma unroll
+  for (int i = 0; i < MAX_PATH_COMPONENTS; i++) {
+
+    dentry_mnt = BPF_CORE_READ(vfsmnt, mnt_root);
+    dentry_parent = BPF_CORE_READ(dentry, d_parent);
+
+    if (dentry == dentry_mnt || dentry == dentry_parent) {
+      if (dentry != dentry_mnt) {
+        // We reached root, but not mount root - escaped?
+        break;
+      }
+      if (mnt != mnt_parent) {
+        // We reached root, but not global root - continue with mount point path
+        dentry = BPF_CORE_READ(mnt, mnt_mountpoint);
+        mnt_parent = BPF_CORE_READ(mnt, mnt_parent);
+        vfsmnt = __builtin_preserve_access_index(&mnt->mnt);
+        continue;
+      }
+      // Global root - path fully parsed
+      break;
+    }
+
+    // Add this dentry name to path
+    name_len = LIMIT_PATH_SIZE(BPF_CORE_READ(dentry, d_name.len));
+    name = BPF_CORE_READ(dentry, d_name.name);
+
+    name_len = name_len + 1; // add slash
+    // Is string buffer big enough for dentry name?
+    if (name_len > buf_off) { break; }
+    volatile size_t new_buff_offset = buf_off - name_len; // satisfy verifier
+    ret = bpf_probe_read_kernel_str(
+      &(out_buf->data[LIMIT_HALF_PERCPU_ARRAY_SIZE(new_buff_offset) // satisfy verifier
+    ]),
+      name_len,
+      name);
+    // ret = bpf_probe_read_kernel_str(
+    //   &(out_buf->data[0]),
+    //   name_len,
+    //   name);
+    if (ret < 0) { return ret; }
+
+    if (ret > 1) {
+      buf_off -= 1;                                    // remove null byte termination with slash sign
+      buf_off = LIMIT_HALF_PERCPU_ARRAY_SIZE(buf_off); // satisfy verifier
+      out_buf->data[buf_off] = '/';
+      buf_off -= ret - 1;
+      buf_off = LIMIT_HALF_PERCPU_ARRAY_SIZE(buf_off); // satisfy verifier
+    } else {
+      // If sz is 0 or 1 we have an error (path can't be null nor an empty string)
+      break;
+    }
+    dentry = dentry_parent;
+  }
+
+  // Is string buffer big enough for slash?
+  if (buf_off != 0) {
+    // Add leading slash
+    buf_off -= 1;
+    buf_off = LIMIT_HALF_PERCPU_ARRAY_SIZE(buf_off); // satisfy verifier
+    out_buf->data[buf_off] = '/';
+  }
+
+  // Null terminate the path string
+  out_buf->data[HALF_PERCPU_ARRAY_SIZE - 1] = 0;
+  *path_str = &out_buf->data[buf_off];
+  return HALF_PERCPU_ARRAY_SIZE - buf_off - 1;
+}
+
+static __noinline int is_str_match(unsigned char *map_path, unsigned char *path) {
+  for (int i = 0; i < 20; i++) {//NAME_MAX
+    if (map_path[i] == '\0')
+      break;
+
+    if (map_path[i] != path[i])
+      return 0;
+  }
+
+  return -EPERM;
+}
+
 static u64 cb_check_path(struct bpf_map *map, u32 *key, struct file_path *map_path, struct callback_ctx *ctx) {
     size_t size = strlen(map_path->path, NAME_MAX);
     if (strcmp(map_path->path, ctx->path, size) == 0) {
@@ -77,27 +200,69 @@ static inline void get_file_info(struct file_open_audit_event *event){
 }
 
 static inline int get_file_perm(struct file_open_audit_event *event,struct file *file){
-    int ret = -1;
+    int ret = 0;//-1;
     int findex = 0;
     struct fileopen_safeguard_config *config = (struct fileopen_safeguard_config *)bpf_map_lookup_elem(&fileopen_safeguard_config_map, &findex);
 
-    if (bpf_d_path(&file->f_path, (char *)event->path, NAME_MAX) < 0) { /* get event->path from file->f_path */
-        return 0;
-    }
-    struct callback_ctx cb = { .path = event->path, .found = false};
-    cb.found = false;
-    bpf_for_each_map_elem(&denied_access_files, cb_check_path, &cb, 0);
-    if (cb.found) {
-        bpf_printk("Access Denied: %s\n", cb.path);
-        ret = -EPERM;
-        goto out;
-    }
+    // struct path *path = __builtin_preserve_access_index(&file->f_path);
+    // get_path_str_from_path(path, event);
+ 
+    struct path *path = __builtin_preserve_access_index(&file->f_path);
+    struct buffer *string_buf = get_buffer();
+    if (string_buf == (void*)0) { return 0; }
+    u_char *file_path = (void*)0;
+    get_path_str_from_path(&file_path, path, string_buf);
+    // bpf_printk("event->path: %s\n", file_path);
+    bpf_probe_read(event->path, sizeof(event->path), file_path);
+    // get_path_str_from_path(&(event->path), path, string_buf);
+    bpf_printk("event->path: %s\n", event->path);
 
-    bpf_for_each_map_elem(&allowed_access_files, cb_check_path, &cb, 0);
-    if (cb.found) {
-        ret = 0;
-        goto out;
-    }
+    // bpf_probe_read_kernel_str(event->path, NAME_MAX, file->f_path.dentry->d_iname);
+
+
+    // if (bpf_d_path(&file->f_path, (char *)event->path, NAME_MAX) < 0) { /* get event->path from file->f_path */
+    //     return 0;
+    // }
+    // struct callback_ctx cb = { .path = event->path, .found = false};
+    // cb.found = false;
+    // bpf_for_each_map_elem(&denied_access_files, cb_check_path, &cb, 0);
+    // if (cb.found) {
+    //     bpf_printk("Access Denied: %s\n", cb.path);
+    //     ret = -EPERM;
+    //     goto out;
+    // }
+
+
+// #pragma unroll
+//     for(int i = 0; i < 3; i++){
+//         //bpf_printk("loop-test: %d\n", i);
+//         u32 index = i;
+//         struct file_path *map_path = (struct file_path *)bpf_map_lookup_elem(&denied_access_files, &index);
+//         if(!map_path) break;
+
+//         // ret = is_str_match(map_path->path, event->path);
+//         // if(ret < 0) {
+//         //     bpf_printk("Access Denied: %s\n", event->path);
+//         //     // ret = -EPERM;
+//         //     break;
+//         // }
+
+//         size_t size = strlen(map_path->path, NAME_MAX);
+//         // bpf_printk("Access Size: %d\n", size);
+//         int str_res = strcmp(map_path->path, event->path, size);
+//         bpf_printk("Access res: %d\n", str_res);
+//         // if (str_res) {
+//         //     //bpf_printk("Access Denied: %s\n", event->path);
+//         //     ret = -EPERM;
+//         //     break;
+//         // }
+//     }
+
+    // bpf_for_each_map_elem(&allowed_access_files, cb_check_path, &cb, 0);
+    // if (cb.found) {
+    //     ret = 0;
+    //     goto out;
+    // }
 
 
 out:
