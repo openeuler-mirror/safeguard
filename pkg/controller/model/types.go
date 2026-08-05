352e4884b09369af2853e14a707dd6e107cc29d5
tongyx12 <tongyx12@chinaunicom.cn>
2026-08-06 06:46:37 +0800

    docs: add godoc comments to model package exports

--- pkg/controller/model/types.go
+++ pkg/controller/model/types.go
@@ -2,6 +2,7 @@ package model
 
 import "time"
 
+// Account represents a user account from /etc/passwd.
 type Account struct {
 	Username string `json:"username"`
 	UID      uint   `json:"uid"`
@@ -10,6 +11,7 @@ type Account struct {
 	Shell    string `json:"shell"`
 }
 
+// RunningProcess represents a currently running process.
 type RunningProcess struct {
 	PID        int    `json:"pid"`
 	Command    string `json:"command"`
@@ -18,6 +20,7 @@ type RunningProcess struct {
 	GID        uint   `json:"gid"`
 }
 
+// HostSnapshot contains all collected data about the host.
 type HostSnapshot struct {
 	Hostname         string           `json:"hostname"`
 	CIDRs            []string         `json:"cidrs"`
@@ -29,25 +32,30 @@ type HostSnapshot struct {
 	Warnings         []string         `json:"warnings"`
 }
 
+// Metadata contains generation metadata for the whitelist.
 type Metadata struct {
 	Hostname    string    `json:"hostname"`
 	GeneratedAt time.Time `json:"generated_at"`
 }
 
+// NetworkWhitelist contains network-related whitelist entries.
 type NetworkWhitelist struct {
 	CIDRAllow []string `json:"cidr_allow"`
 	UIDAllow  []uint   `json:"uid_allow"`
 	GIDAllow  []uint   `json:"gid_allow"`
 }
 
+// FileWhitelist contains file access whitelist entries.
 type FileWhitelist struct {
 	Allow []string `json:"allow"`
 }
 
+// ProcessWhitelist contains process execution whitelist entries.
 type ProcessWhitelist struct {
 	Allow []string `json:"allow"`
 }
 
+// WhitelistModel is the complete whitelist model generated from host data.
 type WhitelistModel struct {
 	Metadata Metadata         `json:"metadata"`
 	Network  NetworkWhitelist `json:"network"`
