46443f689ec000d6ea7ed18fe326db3e33fcf6eb
tongyx12 <tongyx12@chinaunicom.cn>
2026-08-05 10:40:56 +0800

    test: cover cover-process-parse-event

---
+++ pkg/audit/process/parse_event_test.go
@@ -0,0 +1,25 @@
+package process
+
+import (
+	"bytes"
+	"encoding/binary"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParseEvent_ValidEvent(t *testing.T) {
+	var event auditLog
+	event.PID = 9999
+	buf := new(bytes.Buffer)
+	binary.Write(buf, binary.LittleEndian, &event)
+	result, err := parseEvent(buf.Bytes())
+	assert.NoError(t, err)
+	assert.Equal(t, uint32(9999), result.PID)
+}
+
+func TestParseEvent_EmptyBytes(t *testing.T) {
+	_, err := parseEvent([]byte{})
+	assert.Error(t, err)
+}
+
