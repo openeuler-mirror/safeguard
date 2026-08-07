e435f6d1d8602f59871641b4846c3ade8cedbbc2
yusrin <yusj86@chinaunicom.cn>
2026-08-07 11:15:50 +0800

    !491 refactor(mount): correct retToAction identifier spelling
    Signed-off-by: yusrin <yusj86@chinaunicom.cn>

--- pkg/audit/mount/audit.go
+++ pkg/audit/mount/audit.go
@@ -116,7 +116,7 @@ func RunAudit(ctx context.Context, wg *sync.WaitGroup, conf *config.Config) erro
 func newAuditLog(event auditLog) log.RestrictedMountLog {
 	auditEvent := log.AuditEventLog{
 		Module:     MODULE,
-		Action:     retToaction(event.Ret),
+		Action:     retToAction(event.Ret),
 		Hostname:   helpers.NodenameToString(event.Nodename),
 		PID:        event.PID,
 		Comm:       helpers.CommToString(event.Command),
@@ -142,7 +142,7 @@ func parseEvent(eventBytes []byte) (auditLog, error) {
 	return event, nil
 }
 
-func retToaction(ret int32) string {
+func retToAction(ret int32) string {
 	if ret == 0 {
 		return "ALLOWED"
 	} else if ret > 0 {
